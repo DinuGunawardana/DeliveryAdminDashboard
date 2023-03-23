ce123ce5c9e0f13b68b4e77bb343d9f4a47adddc
cbabiomedical <90172658+cbabiomedical@users.noreply.github.com>
2023-03-23 21:43:28 +0530

    Restaurants and Orders CRUD

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { Layout, Image } from "antd";
+import SideMenu from '../src/components/SideMenu';
+import AppRoutes from "./components/AppRoutes";
+import { Amplify } from "aws-amplify";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import awsconfig from './aws-exports';
+import "@aws-amplify/ui-react/styles.css";
+import RestaurantContextProvider from "./contexts/RestaurantContext";
+
+const { Sider, Content, Footer } = Layout;
+
+Amplify.configure(awsconfig);
+
+function App() {
+  return (
+    <RestaurantContextProvider>
+      <Layout>
+        <Sider style={{ height: "100vh", backgroundColor: "white" }}>
+          <Image
+            src="https://alieat-storage82306-staging.s3.ap-northeast-1.amazonaws.com/1.png"
+            preview={false}
+          />
+          <SideMenu />
+        </Sider>
+        <Layout>
+          <Content>
+            <AppRoutes />
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            Restaurant Dashboard ©2023
+          </Footer>
+        </Layout>
+      </Layout>
+    </RestaurantContextProvider>
+  );
+}
+
+export default withAuthenticator(App);
